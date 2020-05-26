e8fde09d059c00cddd3488e522c1bf1fd694b3b9
Nikolay Shevlyakov <cloudo.shifa@gmail.com>
2020-05-26 16:17:39 +0300

    feat: setMakeServer added

--- src/index.tsx
+++ src/index.tsx
@@ -3,5 +3,6 @@ import { load } from './load'
 export { getSpec } from './plugins/mirage/spec'
 
 export { useDevSpec } from './plugins/mirage/const'
+export { setMakeServer } from './plugins/mirage/server'
 
 export default { load, install }
--- src/plugins/mirage/server.ts
+++ src/plugins/mirage/server.ts
@@ -1,6 +1,8 @@
 import { Server } from 'miragejs'
 
-export function makeServer(options: { environment?: 'test' | 'development' }) {
+export function makeServerDefault(options: {
+  environment?: 'test' | 'development'
+}) {
   const { environment = 'test' } = options || {}
 
   return new Server({
@@ -8,7 +10,14 @@ export function makeServer(options: { environment?: 'test' | 'development' }) {
   })
 }
 
+export let makeServer = makeServerDefault
 let server: Server
+
+export const setMakeServer = (
+  makeServerFn: (options: { environment?: 'test' | 'development' }) => Server
+) => {
+  makeServer = makeServerFn
+}
 
 export const devServer = () => {
   if (server) server.shutdown()
